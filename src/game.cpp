6e1f22a451e99e82eb8ab8161475d90b49532a83
nabeelfarooqui98 <nabeelfarooqui98@yahoo.com>
2017-04-20 20:18:02 +0500

    made some basic changes.
    added position IFs.
    fixed cordinates

--- src/game.cpp
+++ src/game.cpp
@@ -68,45 +68,81 @@ void Game::Update()
     // this is for 1 cross
     // sp_cross_1 k liyay positions k liyay karo then again u have to make a texture sp_cross_2
 
-    int i=0;
+    int i=0; //for CROSS ONLY
+    int y=0; // for O, BOT
+
+    /*
+
+    Every time UPDATE is called from the main()
+    it will first remove all pieces from the board (setposition away)
+    then will place one at a time (will increment i after placing one piece)
+    so that if any other IF condition is satisfied it will place next
+    PIECE from the array
+
+    */
+    for(int i=0;i<5;i++)
+    {
+        sp_cross[i].setPosition(-200,0);
+        bsprite[i].setPosition(-200,0);
+    }
+
+    /*
+
+    END OF BLOCK
+
+    */
 
     if(board[0][0]==1)
     {
-        std::cout << "INSIDE if COND\n";
+   //     std::cout << "INSIDE if COND\n";
         sp_cross[i].setPosition(0,0);
+        i++;
     }
     if(board[0][1]==1)
     {
-
         sp_cross[i].setPosition(200,0);
         i++;
     }
     if(board[0][2]==1)
     {
         sp_cross[i].setPosition(400,0);
-    i++;
+        i++;
     }
     if(board[1][0]==1)
     {
         sp_cross[i].setPosition(0,200);
-    i++;
+        i++;
     }
     if(board[1][1]==1)
     {
-        sp_cross[i].setPosition(0,400);
-    i++;
+        sp_cross[i].setPosition(200,200);
+        i++;
     }
     if(board[1][2]==1)
     {
-        sp_cross[i].setPosition(0,600);
-    i++;
+        sp_cross[i].setPosition(400,200);
+        i++;
     }
-
+    if(board[2][0]==1)
+    {
+        sp_cross[i].setPosition(0,400);
+        i++;
+    }
+    if(board[2][1]==1)
+    {
+        sp_cross[i].setPosition(200,400);
+        i++;
+    }
+    if(board[2][2]==1)
+    {
+        sp_cross[i].setPosition(400,400);
+        i++;
+    }
     //for bot
-    int y=0;
+
      if(board[0][0]==2)
     {
-        std::cout << "Entrance of Bot\n";
+//        std::cout << "Entrance of Bot\n";
         bsprite[y].setPosition(0,0);
     }
     if(board[0][1]==2)
@@ -127,15 +163,29 @@ void Game::Update()
     }
     if(board[1][1]==2)
     {
-        bsprite[y].setPosition(0,400);
+        bsprite[y].setPosition(200,200);
     y++;
     }
     if(board[1][2]==2)
     {
+        bsprite[y].setPosition(400,200);
+    y++;
+    }
+    if(board[2][0]==2)
+    {
         bsprite[y].setPosition(0,600);
     y++;
     }
-
+    if(board[2][1]==2)
+    {
+        bsprite[y].setPosition(200,600);
+    y++;
+    }
+    if(board[2][2]==2)
+    {
+        bsprite[y].setPosition(400,600);
+    y++;
+    }
 
     // end of bot
 
