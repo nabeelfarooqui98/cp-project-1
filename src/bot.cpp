4a43c4d678643c3920148ee5a979573eef9fe532
nabeelfarooqui98 <nabeelfarooqui98@yahoo.com>
2017-05-06 22:56:58 +0500

    slight changes
    check the bot class. mujhe amir ka method samaj nai aya tha, board full
    honay ka alag rakh diya. amir ne jo kara tha, us k baad se bot agar
    phele se bhari hui jagha generate kar ta tha to regenerate nai kar raha
    tha

--- src/bot.cpp
+++ src/bot.cpp
@@ -28,19 +28,41 @@ void Bot::Delay(double s)
 
 void Bot::placePiece(int board[3][3])
 {
-    Delay(1);
+    //Delay(1);
+
+
     //it will read the board
     //then set any one 0 to 2
     bool placed = false;
-    bool isempty=true;
+//    bool isempty=true;
     if(type=="EASY")
     {
 
-        while(placed==false&& isempty) //will keep generating random array positions until if finds one that is empty.
+        while(placed==false )//nerating random array positions until if finds one that is empty.
         {
+
+            //all places filled checker
+            bool full = true; //assume that board is FULL
+            for(int i=0;i<3;i++)
+            {
+                for(int j=0;j<3;j++)
+                {
+                    if(board[i][j]==0) //if any cell is empty, set 'full' to false
+                    {
+                        full = false;
+                    }
+                }
+            }
+
+            if(full == true)
+            {
+                std::cout << "board is full" << std::endl;
+                break; //if board is full, bot can not place a piece
+            }
+
             int a = rand()%3;
             int b = rand()%3;
-            isempty=false;
+            //isempty=false;
             std::cout << a << " " << b << std::endl;
 
             if(board[a][b]==0)
