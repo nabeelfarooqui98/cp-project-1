f971adcd3e6c6544f69abff3e9dd7a30779708fc
nabeelfarooqui98 <nabeelfarooqui98@yahoo.com>
2017-05-24 18:14:48 +0500

    mainmenu / music update
    fixed  music
    
    edited main menu picture
    
    cleaned the code
    
    fixed double window issue (game and main menu were being created at the
    same time)

--- main.cpp
+++ main.cpp
@@ -5,53 +5,54 @@
 #include <SFML/Audio.hpp>
 
 int main(){
-    Game game; // Creating our game object.
     int gameState;
     sf::Music music;
+    bool check;
+    bool startgame=false;
+    bool endgame=false;
+
     if (!music.openFromFile("music.ogg"))
     std::cout<<"File Not Found"; // error
-    bool check;
+
+    music.play();// start music
 
 
-     sf::Sprite sp_mainmenu;
+    sf::Sprite sp_mainmenu;
     sf::Texture tex_mainmenu;
     tex_mainmenu.loadFromFile("mainmenu.png");
     sp_mainmenu.setTexture(tex_mainmenu);
-    sp_mainmenu.setPosition(-100,0);
-    sf::RenderWindow window(sf::VideoMode(600, 600), "Tic Tac Toe");
-    bool startgame=false,endgame=false;
-    while (window.isOpen()&&!startgame && !endgame)
+    sp_mainmenu.setPosition(0,0);
+
+    sf::RenderWindow window(sf::VideoMode(600, 600), "Main Menu");
+
+
+    while (window.isOpen() && startgame==false && endgame==false)//both false -> keep main menu
     {
         sf::Event event;
 
         while (window.pollEvent(event))
         {
-        if(event.type == sf::Event::Closed)
-        {
-           endgame = true;
-          window.close();
-        }
+            if(event.type == sf::Event::Closed)
+            {
+                endgame = true;
+                window.close();
+            }
+
             if (event.type == sf::Event::MouseButtonReleased && event.mouseButton.button == sf::Mouse::Left)
                 {
                     int x = sf::Mouse::getPosition(window).x;
                     int y = sf::Mouse::getPosition(window).y;
-                    if((x>0 && y>120) && (x<370 && y<280) )
+                    if((x>0 && y>100) && (x<600 && y<300) )
                     {
-
-                     startgame=true;
+                        startgame=true;
                         window.close();
                     }
-                    if((x>0 && y>300) && (x<370 && y<430) )
+                    if((x>0 && y>300) && (x<600 && y<450) )
                     {
-                     //   amir++;
                         endgame=true;
                         window.close();
                     }
-
-                   // std::cout << x << " -- " << y << std::endl;
-
                 }
-                //window.close();
         }
 
         window.clear();
@@ -60,26 +61,26 @@ int main(){
     }
 
     //comes afterward
-    if(startgame&&!endgame)
+    if(startgame==true && endgame==false)
     {
+        Game game; // Creating our game object.
 
 
-
-    while(game.GetWindow()->IsDone()==false)
-    {
-        // Game loop.
-
-        game.HandleInput();
-        gameState = game.Update(); //will return -1 for draw, 1 for user win, 2 for bot win. 0 for continue
-        game.Render();
-
-        if(gameState != 0)
+        while(game.GetWindow()->IsDone()==false)
         {
-            game.displayMessage(gameState); //send which message to display. this func will also set isDone to true
+            // Game loop.
+
+            game.HandleInput();
+            gameState = game.Update(); //will return -1 for draw, 1 for user win, 2 for bot win. 0 for continue
+            game.Render();
+
+            if(gameState != 0)
+            {
+                game.displayMessage(gameState); //send which message to display. this func will also set isDone to true
+            }
         }
-        music.play();
+
     }
 
-        }
     return 0;
 }
